93465436dbfa447ee13e328ac7d24c99283ddb79
shaloo <5890484+shaloo@users.noreply.github.com>
2023-12-16 12:07:49 +0530

    Refs: #AR-7240 gasless standalone example

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sample-gasless-metamask-only
+
 Sample html/js app integrating with Arcana Gasless SDK supporting third-party wallet MetaMask only
---
+++ index.js
@@ -0,0 +1,90 @@
+//import { arcana } from "https://unpkg.com/@arcana/scw@0.0.22/dist/standalone/scw.umd.js";
+//import { myscw } from "@arcana/scw";
+
+import { erc20abi } from "./erc20.js";
+
+const clientId = "xar_dev_56c6d7f95838926fc7a609e1003bcf31a0b17d51";
+
+await window.ethereum.enable();
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+let signer = await provider.getSigner();
+console.log("EOA: ", await signer.getAddress());
+
+//use import statement - not working
+//const appSCW = new myscw.SCW();
+
+//use scw.umd.js file to instantiate SCW object
+const appSCW = new arcana.scw.SCW();
+
+async function initSCW() {
+  console.log("Intantiating SCW... ");
+  document.querySelector("#result").innerHTML =
+    "Initializing SCW. Please wait...";
+  try {
+    await appSCW.init(clientId, signer);
+    console.log("Init SCW complete!");
+    document.querySelector("#result").innerHTML = "SCW initialized.";
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function getSCWAddress() {
+  console.log("Get SCW Address");
+
+  try {
+    const scwAddress = await appSCW.getSCWAddress();
+    document.querySelector("#result").innerHTML = "SCW address:" + scwAddress;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function gaslessTx() {
+  console.log("Initiating gasless transaction...");
+  try {
+    let amount = ethers.utils.parseUnits("0", 2);
+
+    // Specify the smart contract address as configured via the dashboard
+    const erc20Address = "0xd513E4537510C75E24f941f159B7CAFA74E7B3B9";
+
+    const toAddress = "0xD12E6864A0f0f3Ea886400Ae7570E4341889bDa9";
+    const Erc20Interface = new ethers.utils.Interface(erc20abi);
+
+    // Encode an ERC-20 token transfer to recipientAddress of the specified amount
+
+    // Note: the 'approve' operation  of the smart contract used below
+    // must be whitelisted earlier via the dashboard for gasless transaction
+    const encodedData = Erc20Interface.encodeFunctionData("approve", [
+      toAddress,
+      amount,
+    ]);
+
+    // You need to create transaction objects of the following interface
+    const tx0 = {
+      from: appSCW.getSCWAddress(),
+      to: erc20Address, // destination smart contract address
+      data: encodedData,
+    };
+
+    let tx = await appSCW.doTx(tx0);
+    let txDetails = await tx.wait();
+    console.log("txHash:(CORRECT) " + txDetails.receipt.transactionHash);
+    document.querySelector("#result").innerHTML =
+      "txHash:" + txDetails.receipt.transactionHash;
+  } catch (e) {
+    console.log(e);
+  }
+  console.log("Gasless transaction done!");
+}
+
+document.querySelector("#Btn-Init-Auth").addEventListener("click", initSCW);
+
+document
+  .querySelector("#Btn-Get-SCW-Address")
+  .addEventListener("click", getSCWAddress);
+
+document
+  .querySelector("#Btn-Do-Gasless-Tx")
+  .addEventListener("click", gaslessTx);
+
